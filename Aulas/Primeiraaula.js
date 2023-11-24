960b7bf1d34929e06db662eb81f3914035f13810
Fredbortolon <fredbortolon@gmail.com>
2023-11-23 22:54:53 -0300

    Primeira Aula

---
+++ Aulas/Primeiraaula.js
@@ -0,0 +1,10 @@
+const numero1 = 1;
+let numero2 = 2;
+// CONST = Constante, ele nunca vai mudar. - Fica mais leve na hora de salvar no computador.
+// LET = Variável.
+
+if(numero1 === 1){
+    numero2 = 6
+}
+
+console.log('resultado',numero2)
