9201e76bcdb3cf8522ac92438f0a163ef08df7c9
Fredbortolon <fredbortolon@gmail.com>
2023-11-24 11:17:57 -0300

    Aula04-ex001.commit

---
+++ Segundaaula.js
@@ -0,0 +1,24 @@
+const idade = 8;
+let = podeentrar = false;
+
+if(idade >= 18){
+podeentrar = true;
+} else{
+    console.log('NÃO ENTRA!')
+}
+
+console.log('usuario pode entrar', podeentrar)
+
+//////////////////////////
+
+const x = 3
+const y = 4
+const idade = 22
+const verdade = false
+const tempo = chuva
+
+console.log(x)
+console.log(y)
+console.log(idade)
+console.log(verdade)
+console.log(tempo)
